4cb483b6d4949b850f2666a61aa20ed6ca803b0f
lisantov <lisantov@yandex.ru>
2025-10-17 23:01:48 +0700

    Add submit button component and simple file handler to input in scripts/parser

--- scripts/components.js
+++ scripts/components.js
@@ -13,3 +13,21 @@ export class Preloader {
         this.element.classList.remove('loading');
     }
 }
+
+export class SubmitButton {
+    element;
+    constructor() {
+        this.element = document.querySelector('.parser-form__button');
+    }
+
+
+    setButtonActive = () => {
+        this.element.classList.remove('parser-form__button--disabled');
+        this.element.classList.add('parser-form__button--active');
+    }
+
+    setButtonDisabled = () => {
+        this.element.classList.remove('parser-form__button--active');
+        this.element.classList.add('parser-form__button--disabled');
+    }
+}
--- scripts/parser.js
+++ scripts/parser.js
@@ -1,10 +1,28 @@
-import { Preloader } from './components';
-import { isFileIsJson, readFileAsText } from './utils';
+import { Preloader, SubmitButton } from './components.js';
+import { isFileIsJson, readFileAsText } from './utils.js';
 
+// ЭЛЕМЕНТЫ
 const form = document.querySelector('.parser-form');
+const formErrorText = document.querySelector('.parser-form__error');
 const formFileInput = document.querySelector('.parser-form__input');
 
 const preloaderTemplate = document.getElementById('preloader-template');
+
+// КОМПОНЕНТЫ
 const preloader = new Preloader(preloaderTemplate.content);
+const submitButton = new SubmitButton();
 
 let currentFile;
+
+const handleFileChange = (e) => {
+    if(isFileIsJson(e.target.files[0])) {
+        formErrorText.textContent = '';
+        submitButton.setButtonActive();
+    }
+    else {
+        formErrorText.textContent = 'Невалидный формат файла. Принимается только файлы с содержанием JSON';
+        submitButton.setButtonDisabled();
+    }
+}
+
+formFileInput.addEventListener('change', handleFileChange);
