e46eab1df335c1c008debd5502e088624dc17125
lisantov <lisantov@yandex.ru>
2025-10-22 12:58:45 +0700

    Change select field to create checkboxes or radio buttons in dependency on multiple attribute in scripts/component.js. Add new styles for checkboxes and radio buttons

--- scripts/components.js
+++ scripts/components.js
@@ -39,7 +39,7 @@ export class InputField {
         this.element = document.createElement('input');
         this.element.classList.add('result-form__input');
 
-        if(attrs.type === 'file') this.element.classList.add('result-form__input--hidden');
+        if(attrs.type === 'file') this.element.classList.add('hidden-element');
         this.setAttributes(attrs);
     }
 
@@ -48,7 +48,7 @@ export class InputField {
 
         keys.forEach((key) => {
             if (key === 'filetype') {
-                this.element.addEventListener('input', () => {
+                this.element.addEventListener('change', () => {
                     if (!areFilesValid(this.element.files, attrs[key])) {
                         this.element.value = '';
                         alert('Неподходящий формат файла');
@@ -64,6 +64,8 @@ export class InputField {
                 this.element.addEventListener('keydown', (e) =>{
                     if(e.key === 'Backspace') {
                         const rawValue = getRawNumbers(e.target.value);
+
+                        // Не получится удалить конкретный символ, всё удаляется с конца строки
                         this.element.value = maskValue(rawValue.slice(0, rawValue.length - 1), mask);
                     }
                 })
@@ -75,19 +77,33 @@ export class InputField {
 
 export class SelectField {
     element;
-    constructor(attrs) {
-        this.element = document.createElement('select');
-        this.element.classList.add('result-form__select');
+    choiceContainer;
+    constructor(attrs, name) {
+        this.element = document.createElement('div');
+        this.element.classList.add('result-form__select-wrapper');
+
+        const title = document.createElement('p');
+        title.classList.add('result-form__label');
+        title.textContent = name;
+
+        this.element.appendChild(title);
+
+        this.choiceContainer = document.createElement('div');
+        this.choiceContainer.classList.add('result-form__select-container');
+
         this.setAttributes(attrs);
 
-        if(attrs.type === 'color') {
-            this.actualizeBackgroundColor();
-            this.element.addEventListener('change', () => this.actualizeBackgroundColor());
-        }
-    }
+        this.element.appendChild(this.choiceContainer);
 
-    actualizeBackgroundColor() {
-        this.element.style.backgroundColor = this.element.options[this.element.selectedIndex].value
+        if (!attrs.multiple)
+            this.choiceContainer.addEventListener("click", (e) => {
+                e.stopPropagation();
+                this.choiceContainer.childNodes.forEach(label => {
+                    if(label.textContent === e.target.value) {
+                        label.classList.add('result-form__select--checked');
+                    } else label.classList.remove('result-form__select--checked');
+                })
+            })
     }
 
     setAttributes(attrs) {
@@ -95,15 +111,31 @@ export class SelectField {
 
         keys.forEach((key) => {
             if(!Array.isArray(attrs[key])) {
-                this.element.setAttribute(key, attrs[key]);
+                this.choiceContainer.setAttribute(key, attrs[key]);
             }
             else {
                 attrs[key].forEach((option) => {
-                    const optionElement = document.createElement('option');
+                    const labelElement = document.createElement('label');
+                    labelElement.classList.add('result-form__select');
+                    if (attrs.type === 'color') labelElement.style.backgroundColor = option;
+                    labelElement.textContent = option;
+
+                    const optionElement = document.createElement('input');
+                    attrs.multiple ? optionElement.type = 'checkbox' : optionElement.type = 'radio';
+                    optionElement.classList.add('hidden-element');
                     optionElement.value = option;
-                    if (attrs.type === 'color') optionElement.style.backgroundColor = option;
-                    else optionElement.textContent = option;
-                    this.element.appendChild(optionElement);
+                    optionElement.name = 'select';
+                    if (attrs.multiple)
+                        optionElement.addEventListener("click", (e) => {
+                            e.stopPropagation();
+                            return optionElement.checked ?
+                                labelElement.classList.add('result-form__select--active') :
+                                labelElement.classList.remove('result-form__select--active')
+                        });
+
+                    labelElement.appendChild(optionElement);
+
+                    this.choiceContainer.appendChild(labelElement);
                 })
             }
         })
@@ -112,13 +144,16 @@ export class SelectField {
 
 export class Input {
     element;
+    isSelect;
     constructor(fieldObject) {
         this.element = document.createElement('label');
         this.element.classList.add('result-form__label');
         this.element.textContent = fieldObject.label;
 
-        const field = (isThereArrayInObject(fieldObject.input) && fieldObject.input.type !== 'file') ?
-            new SelectField(fieldObject.input) :
+        this.isSelect = isThereArrayInObject(fieldObject.input) && fieldObject.input.type !== 'file';
+
+        const field = this.isSelect ?
+            new SelectField(fieldObject.input, fieldObject.label) :
             new InputField(fieldObject.input);
 
         if (fieldObject.input.type === 'file') {
@@ -142,13 +177,17 @@ export class Input {
 
             this.element.addEventListener("drop", (e) => {
                 e.preventDefault();
-                this.element.classList.remove("parser-form__custom-input--active");
+                this.element.classList.remove("result-form__custom-input--active");
                 field.element.files = e.dataTransfer.files;
                 field.element.dispatchEvent(new Event('change'));
+                const topFiles = Array.from(field.element.files).map((file) => file.name);
+                this.element.textContent = `🔗 Выбрано: ${topFiles[0]} ${topFiles.length > 1 ? `и ещё(${topFiles.length - 1})` : ''}`;
             })
         }
 
-        this.element.appendChild(field.element);
+        this.isSelect ?
+            this.element = field.element :
+            this.element.appendChild(field.element);
     }
 }
 
