c11b55a8dd13b77eab790e422445ca46bb4065ed
lisantov <lisantov@yandex.ru>
2025-10-27 23:50:49 +0700

    Add scroll by blocks for desktop with some issues, going to fix it.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,3 +7,40 @@ import './animations/hero.js';
 import './animations/steps.js';
 import './animations/promo.js';
 import './animations/feedback.js';
+
+const main = document.querySelector('.main');
+const header = document.querySelector('.header');
+const footer = document.querySelector('.footer');
+const blocks = [header, ...main.children, footer];
+let currentBlock = 0;
+let isScrollBlocked = false;
+
+let lastScrollPosition = 0;
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'pageUp' || e.key === 'pageDown') e.preventDefault();
+});
+
+document.onscroll = (e) => {
+    e.preventDefault();
+    const currentScrollPosition = window.scrollY;
+
+    if (!isScrollBlocked && currentScrollPosition > lastScrollPosition) {
+        if (currentBlock < blocks.length - 1) {
+            isScrollBlocked = true;
+            currentBlock++;
+            location.hash = "#" + blocks[currentBlock].id;
+            setTimeout(() => isScrollBlocked = false, 700);
+        }
+    }
+    else {
+        if (!isScrollBlocked && currentBlock > 0) {
+            isScrollBlocked = true;
+            currentBlock--;
+            location.hash = "#" + blocks[currentBlock].id;
+            setTimeout(() => isScrollBlocked = false, 700);
+        }
+    }
+
+    lastScrollPosition = currentScrollPosition;
+};
