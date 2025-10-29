e39a768c806fca1753da16a42e2646c4bc2e3f20
lisantov <lisantov@yandex.ru>
2025-10-29 13:28:48 +0700

    Fix some of animations. Delete scroll by blocks

--- src/scripts/animations/hero.js
+++ src/scripts/animations/hero.js
@@ -1,20 +1,13 @@
 const cover = document.querySelector('.hero__cover');
 const outTrigger = document.getElementById('hero-out');
-const inTrigger = document.querySelector('.hero');
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        if (!entry.isIntersecting && entry.target.id === "hero-out") {
+        if (!entry.isIntersecting) {
             cover.classList.add('hero__cover--out');
             return;
         }
-        else if (entry.isIntersecting && entry.target.id !== "hero-out") {
-            cover.classList.add('hero__cover--in');
-            return;
-        }
-        cover.classList.remove('hero__cover--in')
         cover.classList.remove('hero__cover--out');
     });
 });
-observer.observe(inTrigger);
 observer.observe(outTrigger);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,39 +8,39 @@ import './animations/steps.js';
 import './animations/promo.js';
 import './animations/feedback.js';
 
-const main = document.querySelector('.main');
-const header = document.querySelector('.header');
-const footer = document.querySelector('.footer');
-const blocks = [header, ...main.children, footer];
-let currentBlock = 0;
-let isScrollBlocked = false;
-
-let lastScrollPosition = 0;
-
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'pageUp' || e.key === 'pageDown') e.preventDefault();
-});
-
-document.onscroll = (e) => {
-    e.preventDefault();
-    const currentScrollPosition = window.scrollY;
-
-    if (!isScrollBlocked && currentScrollPosition > lastScrollPosition) {
-        if (currentBlock < blocks.length - 1) {
-            isScrollBlocked = true;
-            currentBlock++;
-            location.hash = "#" + blocks[currentBlock].id;
-            setTimeout(() => isScrollBlocked = false, 700);
-        }
-    }
-    else {
-        if (!isScrollBlocked && currentBlock > 0) {
-            isScrollBlocked = true;
-            currentBlock--;
-            location.hash = "#" + blocks[currentBlock].id;
-            setTimeout(() => isScrollBlocked = false, 700);
-        }
-    }
-
-    lastScrollPosition = currentScrollPosition;
-};
+// const main = document.querySelector('.main');
+// const header = document.querySelector('.header');
+// const footer = document.querySelector('.footer');
+// const blocks = [header, ...main.children, footer];
+// let currentBlock = 0;
+// let isScrollBlocked = false;
+//
+// let lastScrollPosition = 0;
+//
+// document.addEventListener('keydown', (e) => {
+//     if (e.key === 'pageUp' || e.key === 'pageDown') e.preventDefault();
+// });
+//
+// document.onscroll = (e) => {
+//     e.preventDefault();
+//     const currentScrollPosition = window.scrollY;
+//
+//     if (!isScrollBlocked && currentScrollPosition > lastScrollPosition) {
+//         if (currentBlock < blocks.length - 1) {
+//             isScrollBlocked = true;
+//             currentBlock++;
+//             location.hash = "#" + blocks[currentBlock].id;
+//             setTimeout(() => isScrollBlocked = false, 700);
+//         }
+//     }
+//     else {
+//         if (!isScrollBlocked && currentBlock > 0) {
+//             isScrollBlocked = true;
+//             currentBlock--;
+//             location.hash = "#" + blocks[currentBlock].id;
+//             setTimeout(() => isScrollBlocked = false, 700);
+//         }
+//     }
+//
+//     lastScrollPosition = currentScrollPosition;
+// };
